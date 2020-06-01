9cbbc455bcb15a3fd4333c35b731502d8a7d228a
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-01 15:56:23 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,5 +1,9 @@
 def using_until
+<<<<<<< HEAD
   levitation_force = 6
+=======
+  levitation_force = 0
+>>>>>>> 582bd23953fb703178f91c9b4b3ff5b743f0ceb4
     until levitation_force == 10
       puts "Wingardium Leviosa"
       levitation_force += 1
